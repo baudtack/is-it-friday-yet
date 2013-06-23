f8084748cf9f8a5e86fa9b5646bae33bd43216ae
David Kerschner <baudtack@baudtack.com>
2013-06-22 21:58:37 -0700

    Made it suck less

--- is-it-friday-yet.scm
+++ is-it-friday-yet.scm
@@ -1,22 +1,14 @@
 (use awful)
 (use html-tags)
 
-(enable-sxml #t)
-
 (define-page (main-page-path)
   (lambda ()
     (<h1> style: "font-size:1750%;text-align:center;" 
           (let ((today (vector-ref (seconds->local-time) 6)))
-            (if (eq? today 5)
+            (if (eqv? today 5)
                 "YES"
                 (string-append "No, it's " (day-of-week today) "."))))))
     
 (define day-of-week
   (lambda (numeric-day-of-week)
-    (cond ((eq? numeric-day-of-week 0) "Sunday")
-          ((eq? numeric-day-of-week 1) "Monday")
-          ((eq? numeric-day-of-week 2) "Tuesday")
-          ((eq? numeric-day-of-week 3) "Wednesday")
-          ((eq? numeric-day-of-week 4) "Thursday")
-          ((eq? numeric-day-of-week 5) "Friday")
-          ((eq? numeric-day-of-week 6) "Saturday"))))
+    (list-ref '("Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday") numeric-day-of-week)))
